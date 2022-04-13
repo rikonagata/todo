85069d1d6fed24acf737a34451dccec66117ee09
rikonagata <103544713+rikonagata@users.noreply.github.com>
2022-04-13 17:01:40 +0900

    １

--- src/index.js
+++ src/index.js
@@ -15,11 +15,42 @@ const onClickAdd = () => {
   //console.log(li);
   //alert(inputText);
 
+  //buttonタグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    //押された完了ボタンの親タグ(div)を未完了リストから削除
+    deleteFromIncompleteList(deleteButton.parentNode);
+
+    //完了リストに追加する要素
+    const addTarget = completeButton.parentNode;
+    //TODO内容テキストを取得
+    const text = addTarget.firstElementChild.innerText;
+    //div以下を初期化
+    addTarget.textContent = null;
+
+    //liタグを初期化
+  });
+
+  //buttonタグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //押された削除ボタンの親タグ(div)を未完了リストから削除
+    deleteFromIncompleteList(deleteButton.parentNode);
+  });
+
   //divタグの子要素に各要素を設定
   div.appendChild(li);
-
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
   //未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
+};
+
+//未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
