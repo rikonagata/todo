d80b2d76f91b8ac012b3c76581aea7e203cab997
rikonagata <103544713+rikonagata@users.noreply.github.com>
2022-04-13 15:32:44 +0900

    div

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import "./styles.css";
 
-const onlClickAdd () =>{
-   
-}
+const onClickAdd = () => {
+  //テキストボックスの値を取得し初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //liタグ
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  //console.log(li);
+  //alert(inputText);
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+  console.log(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
